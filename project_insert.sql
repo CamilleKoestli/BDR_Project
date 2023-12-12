3bde914ebcc6f1ef8ee1d8fcb921e349a163e894
CamilleKoestli <camille.koestli@heig-vd.ch>
2023-12-12 13:37:11 +0100

    insert, request, triggers

---
+++ project_insert.sql
@@ -0,0 +1,10 @@
+--UTILISATEURS
+-- Insérer le premier utilisateur
+INSERT INTO "Utilisateur" (pseudo, motdepasse, adressemail) VALUES ('john_doe', 'motdepasse1', 'john.doe@example.com');
+
+-- Insérer le deuxième utilisateur
+INSERT INTO "Utilisateur" (pseudo, motdepasse, adressemail) VALUES ('jane_smith', 'motdepasse2', 'jane.smith@example.com');
+--END
+
+--ARTISTES
+--END
