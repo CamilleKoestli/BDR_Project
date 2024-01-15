f766fc9b709fb75601a092a299bc453c82341fe5
CamilleKoestli <camille.koestli@heig-vd.ch>
2024-01-15 16:26:26 +0100

    class query

---
+++ photoz/app/src/main/java/photoz/database/Query.java
@@ -0,0 +1,39 @@
+package photoz.database;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class Query {
+
+    static ResultSet query(String sql) throws SQLException {
+        Connection conn = PostgresConnection.getInstance().getConnection();
+        PreparedStatement preparedStatement = conn.prepareStatement(sql);
+        ResultSet resultSet = preparedStatement.executeQuery();
+
+        return resultSet;
+    }
+
+    public ResultSet query(String sql, Object[] params) throws SQLException {
+        Connection conn = PostgresConnection.getInstance().getConnection();
+        PreparedStatement preparedStatement = conn.prepareStatement(sql);
+
+        for (Object param : params) {
+            int cnt = 1;
+            if (param instanceof String p) {
+                preparedStatement.setString(cnt++, p);
+            }
+            if (param instanceof Integer p) {
+                preparedStatement.setInt(cnt++, p);
+            }
+            if (param instanceof Double p) {
+                preparedStatement.setDouble(cnt++, p);
+            }
+        }
+
+        ResultSet resultSet = preparedStatement.executeQuery();
+
+        return resultSet;
+    }
+}
