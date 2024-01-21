eec0ac19f2c6dbaf5142f37c248bcfb8da08d8d3
CamilleKoestli <camille.koestli@heig-vd.ch>
2024-01-21 13:22:22 +0100

    map du statut

--- photoz/app/src/main/java/photoz/models/Statut.java
+++ photoz/app/src/main/java/photoz/models/Statut.java
@@ -1,15 +1,38 @@
 package photoz.models;
 
-public class Statut {
-    private String pseudo;
-    private String pseudoart;
-    private boolean typedemande; //suivre true     abonner false
-    private boolean accepte_refus; //accepté true     refus false
+import photoz.database.Query;
 
+import java.sql.ResultSet;
+
+import java.sql.SQLException;
+
+public class Statut {
+    public String pseudo;
+    public String pseudoart;
+    public boolean typedemande;
+    public boolean accepte_refus;
+
+    public Statut() {
+    }
     public Statut(boolean typedemande, boolean accepte_refus, String pseudoart, String pseudo) {
         this.pseudo = pseudo;
         this.pseudoart = pseudoart;
         this.typedemande = accepte_refus;
         this.accepte_refus = accepte_refus;
+    }
+
+    public static Statut mapResultSetToStatut(ResultSet set) {
+        Statut s = new Statut();
+        try {
+            s.pseudo = set.getString("pseudo");
+            s.pseudoart = set.getString("pseudoart");
+            s.typedemande = set.getBoolean("typedemande");
+            s.accepte_refus = set.getBoolean("accepte_refus");
+
+            return s;
+        } catch (SQLException e) {
+            System.out.println(e);
+            return null;
+        }
     }
 }
